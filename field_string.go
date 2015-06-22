5ddc82179e298faf61634fb8336fe5a5c1fad18c
Cheney <me@chenye.org>
2015-06-22 11:53:09 +0800

    add string arg default

--- field_string.go
+++ field_string.go
@@ -59,8 +59,9 @@ func (b *StringField) SetArg(v *reflect.Value, arg string) error {
 		return fmt.Errorf("value %v is not settable", v)
 	}
 
-	if arg != "" {
-		v.Set(reflect.ValueOf(arg))
+	if arg == "" {
+		arg = b.Default().(string)
 	}
+	v.Set(reflect.ValueOf(arg))
 	return nil
 }
